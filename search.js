3c91dc2e88eeb23a899b37eb616cbcdc526adf0d
Emre Şanlı <memres@gmail.com>
2019-05-26 18:57:19 +0300

    Update search.js

--- search.js
+++ search.js
@@ -1,14 +1,16 @@
-var suggestCallBack;
+var callback, rotate = 'rotation 2s infinite linear';
 $(function() {
-	$('input').autocomplete({
+	$('input[type="search"]').autocomplete({
+		appendTo: 'article',
+		autoFocus: true,
 		source: function(request, response) {
 			$.getJSON('https://suggestqueries.google.com/complete/search?callback=?', {
 				'ds': 'yt',
 				'client': 'youtube',
-				'jsonp': 'suggestCallBack',
+				'jsonp': 'callback',
 				'q': request.term
 			});
-			suggestCallBack = function(data) {
+			callback = function(data) {
 				var suggestions = [];
 				$.each(data[1], function(key, val) {
 					suggestions.push({
@@ -19,12 +21,12 @@ $(function() {
 				response(suggestions);
 			};
 		},
-	});
-	$('button[type="button"]').on('click', function() {
-		$(this).css('pointer-events', 'none').find('i').css('animation', 'rotation 2s infinite linear');
-		$(location).attr('href', $(this).attr('value'));
+		select: function(event, ui) {
+			$('button[type="submit"] i').css('animation', rotate);
+			location.href = '?q=' + encodeURI(ui.item.value).replace(/%20/g, '+');
+		}
 	});
 	$('form').on('submit', function() {
-		$('button[type="submit"]').css('pointer-events', 'none').find('i').css('animation', 'rotation 2s infinite linear');
+		$('button[type="submit"] i').css('animation', rotate);
 	});
 });
