996d752a81e94a995c6bbbe5149ee21d453c1b14
Emre Şanlı <memres@gmail.com>
2019-10-28 05:47:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function() {
 		script.src = 'https://cdn.jsdelivr.net/npm/shake.js@1.2.2/shake.min.js';
 		document.head.appendChild(script);
 		script.onload = function() {
-			var shakeEvent = new Shake({threshold: 23});
+			var shakeEvent = new Shake({threshold: 30});
 			shakeEvent.start();
 			window.addEventListener('shake', next, false);
 		}
@@ -14,42 +14,28 @@ $(function() {
 	$('[name="country"]').val(Cookies.get('country'));
 	if (Cookies.get('shuffle')) $('.shuffle').addClass('on');
 	var folder = window.location.pathname.split('/').slice(0, -1).join('/'),
+	itag = /iPhone|iPad|iPod/i.test(navigator.userAgent) ? 140 : 251,
 	audio = $('audio')[0],
 	track = Cookies.get('shuffle') ? randomize() : 0,
 	trax = [track],
 	timeout;
+	if (itag == 140) $('.download').remove();
 	play(track);
+	$('.play').on('click', playpause);
+	$('.next').on('click', next);
+	$('.prev').on('click', prev);
+	$('.queue').on('click', queue);
+	$('.shuffle').on('click', shuffle);
+	$('.download').on('click', download);
 	$('select').on('change', function() {
 		Cookies.set($(this).attr('name'), this.value, {expires: 365, path: folder});
 		location.reload();
 	});
-	$('.prev').on('click', prev);
-	$('.next').on('click', next);
-	$('.play').on('click', function() {
-		audio.paused ? audio.play() : audio.pause();
-	});
 	$(document).on('click', '.elli', function() {
 		$(this).toggleClass('psis');
 	});
-	$('.loop, .shuffle').on('click', function() {
+	$('.loop').on('click', function() {
 		$(this).toggleClass('on');
-	});
-	$('.shuffle').on('click', function() {
-		$(this).hasClass('on') ? Cookies.set('shuffle', 1, {expires: 365, path: folder}) : Cookies.remove('shuffle', {path: folder});
-	});
-	$('.queue').on('click', function() {
-		let h1 = $('li.on').text();
-		if ($(this).hasClass('on')) {
-			$(this).removeClass('on');
-			$('h1').text(h1);
-			$('ul').slideUp();
-		}
-		else {
-			$(this).addClass('on');
-			$('h1').text('QUEUE');
-			$('ul').slideDown();
-			$('.elli').addClass('psis');
-		}
 	});
 	$('li').on('click', function() {
 		if ($(this).hasClass('on')) {
@@ -62,66 +48,12 @@ $(function() {
 			trax.push(track);
 		}
 	});
-	$('figure').slider({
-		value: audio.currentTime,
-		slide: function(event, ui) {
-			audio.currentTime = (audio.duration / 100) * ui.value;
-		}
-	});
-	$('.ui-slider-handle').off('keydown');
-	$(window).on('keydown', function(event) {
-		if ($(event.target).is('INPUT')) return;
-		if (event.which == 37) audio.currentTime = audio.currentTime - 3;
-		if (event.which == 39) audio.currentTime = audio.currentTime + 3;
-		if (event.which == 38 && audio.volume < 1) audio.volume = (Math.round(audio.volume * 100) / 100) + 0.05;
-		if (event.which == 40 && audio.volume > 0) audio.volume = (Math.round(audio.volume * 100) / 100) - 0.05;
-	});
-	$(window).on('keyup', function(event) {
-		if ($(event.target).is('INPUT')) return;
-		if (event.which == 32) audio.paused ? audio.play() : audio.pause();
-		if (event.which == 13) next();
-		if (event.which == 80) prev();
-		if (event.which == 76) $('.loop').toggleClass('on');
-		if (event.which == 83) {
-			$('.shuffle').toggleClass('on');
-			$('.shuffle').hasClass('on') ? Cookies.set('shuffle', 1, {expires: 365, path: folder}) : Cookies.remove('shuffle', {path: folder});
-		}
-		if (event.which == 81) {
-			let h1 = $('li.on').text();
-			if ($('.queue').hasClass('on')) {
-				$('.queue').removeClass('on');
-				$('h1').html(h1);
-				$('ul').slideUp();
-			}
-			else {
-				$('.queue').addClass('on');
-				$('h1').html('Queue');
-				$('ul').slideDown();
-				$('.elli').addClass('psis');
-			}
-		}
-	});
 	$('audio').on('volumechange', function() {
 		clearTimeout(timeout);
 		$('header').stop(true, true).show().find('i').text(audio.volume == 0 ? 'volume_off' : (audio.volume <= 0.5 ? 'volume_down' : 'volume_up')).next().text(Math.round(audio.volume * 100));
 		timeout = setTimeout(function() {
 			$('header').fadeOut();
 		}, 1000);
-	});
-	$('audio').on('error', function() {
-		var id = $('li.on').attr('id');
-		if ($('audio[src*="drycat"]').length) {
-			audio.src = 'https://invidio.us/latest_version?local=true&itag=251&id=' + id;
-			audio.pause();
-			audio.load();
-			audio.play();
-		}
-		else {
-			$('li.on').remove();
-			if ($('.shuffle').hasClass('on')) track = randomize();
-			else if (track == $('li:last-child').index()) track = 0;
-			play(track);
-		}
 	});
 	$('audio').on('loadedmetadata', function() {
 		$('.total').text(calc(audio.duration));
@@ -152,13 +84,20 @@ $(function() {
 		if ($('.loop').hasClass('on')) audio.play();
 		else next();
 	});
-	function calc(d, c) {
-		var hour = parseInt(d / 3600),
-		min = parseInt((d / 60) % 60),
-		sec = parseInt(d - ((hour * 3600) + (min * 60)));
-		if (c) sec = parseInt(d % 60);
-		return (hour ? hour + ':' : '') + (hour && min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
-	}
+	$('audio').on('error', function() {
+		if ($('audio[src*="invidio.us"]').length) {
+			audio.src = 'https://invidiou.sh/latest_version?itag=' + itag + '&id=' + $('li.on').attr('id');
+			audio.pause();
+			audio.load();
+			audio.play();
+		}
+		else {
+			$('li.on').remove();
+			if ($('.shuffle').hasClass('on')) track = randomize();
+			else if (track == $('li:last-child').index()) track = 0;
+			play(track);
+		}
+	});
 	function play(n) {
 		var li = $('li:eq(' + n + ')'),
 		title = li.text(),
@@ -184,10 +123,17 @@ $(function() {
 			}
 		});
 		//
-		audio.src = 'https://invidious.drycat.fr/latest_version?local=true&itag=251&id=' + id;
+		audio.src = 'https://invidio.us/latest_version?local=true&itag=' + itag + '&id=' + id;
 		audio.pause();
 		audio.load();
 		audio.play();
+	}
+	function calc(d, c) {
+		var hour = parseInt(d / 3600),
+		min = parseInt((d / 60) % 60),
+		sec = parseInt(d - ((hour * 3600) + (min * 60)));
+		if (c) sec = parseInt(d % 60);
+		return (hour ? hour + ':' : '') + (hour && min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
 	}
 	function next() {
 		if ($('.shuffle').hasClass('on')) track = randomize();
@@ -217,4 +163,60 @@ $(function() {
 		}
 		else return num;
 	}
+	function playpause() {
+		audio.paused ? audio.play() : audio.pause();
+	}
+	function shuffle() {
+		$('.shuffle').toggleClass('on');
+		$('.shuffle').hasClass('on') ? Cookies.set('shuffle', 1, {expires: 365, path: folder}) : Cookies.remove('shuffle', {path: folder});
+	}
+	function download() {
+		window.location.href = 'https://alltube.drycat.fr/download?audio=on&url=http%3A%2F%2Fyoutu.be%2F' + $('li.on').attr('id');
+	}
+	function queue() {
+		let h1 = $('li.on').text();
+		if ($('.queue').hasClass('on')) {
+			$('.queue').removeClass('on');
+			$('h1').text(h1);
+			$('ul').slideUp();
+		}
+		else {
+			$('.queue').addClass('on');
+			$('h1').text('QUEUE');
+			$('ul').slideDown();
+			$('.elli').addClass('psis');
+		}
+	}
+	$('ul').sortable({
+		handle: 'span',
+		update: function() {
+			if (Cookies.get('shuffle')) shuffle();
+			track = $('li.on').index();
+			trax = [track];
+		}
+	});
+	$('ul').disableSelection();
+	$('figure').slider({
+		step: .01,
+		value: audio.currentTime,
+		slide: function(event, ui) {
+			audio.currentTime = (audio.duration / 100) * ui.value;
+		}
+	});
+	$('.ui-slider-handle').off('keydown');
+	$(window).on('keydown', function(event) {
+		if (event.which == 37) audio.currentTime = audio.currentTime - 3;
+		if (event.which == 39) audio.currentTime = audio.currentTime + 3;
+		if (event.which == 38 && audio.volume < 1) audio.volume = (Math.round(audio.volume * 100) / 100) + 0.05;
+		if (event.which == 40 && audio.volume > 0) audio.volume = (Math.round(audio.volume * 100) / 100) - 0.05;
+	});
+	$(window).on('keyup', function(event) {
+		if (event.which == 32) playpause();
+		if (event.which == 13) next();
+		if (event.which == 80) prev();
+		if (event.which == 76) $('.loop').toggleClass('on');
+		if (event.which == 83) shuffle();
+		if (event.which == 81) queue();
+		if (event.which == 68 && itag == 251) download();
+	});
 });
